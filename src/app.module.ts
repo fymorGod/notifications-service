ffdbcf212dcca00dcdd011396e39623678755226
fymorGod <mufinamgaer@gmail.com>
2022-12-13 08:37:31 -0300

    ignite lab

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { HttpModule } from './http.module';
+import { MailService } from './mail/mail.service';
+import { SMTPMailService } from './mail/smtp-mail.service';
+
+@Module({
+  imports: [
+    HttpModule
+  ],
+  controllers: [AppController],
+  providers: [
+    {
+      provide: MailService,
+      useClass: SMTPMailService
+    }
+  ],
+})
+export class AppModule {}
